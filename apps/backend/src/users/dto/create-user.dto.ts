f1ba65a45f4baf84c34f6cd7b93e91a8cec923d0
martonbakk <marci.bakk2004@gmail.com>
2024-05-09 18:15:30 +0200

    dto es entity

--- apps/backend/src/users/dto/create-user.dto.ts
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { OmitType } from '@nestjs/swagger';
+
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends OmitType(User, [
+  'authSchId',
+  'email',
+  'name',
+  'isAdmin',
+  'ownedEvents',
+  'organizedEvents',
+]) {}
--- apps/backend/src/users/entities/user.entity.ts
+++ apps/backend/src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class User {
+  @IsString()
+  @IsNotEmpty()
+  authSchId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  nickname: string;
+  phone: string;
+
+  @IsBoolean()
+  isAdmin: boolean;
+
+  ownedEvents: Event[];
+  organizedEvents: Event[];
+}
