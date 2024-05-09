f578d6284399305c8c901170528e5851a3113afb
Bakonyi -Nemes Attila <bakonyiattilaksi04@gmail.com>
2024-05-09 16:35:44 +0200

    crud restart

--- apps/backend/src/event/event.controller.ts
+++ apps/backend/src/event/event.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 
 import { EventService } from './event.service';
@@ -6,6 +6,19 @@ import { EventService } from './event.service';
 @Controller('events')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
+  @Post()
+  create(@Body() data: Prisma.EventCreateInput) {
+    return this.eventService.create(data);
+  }
+  @Get()
+  findAll() {
+    return this.eventService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventService.findOne(id);
+  }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() data: Prisma.EventUpdateInput) {
