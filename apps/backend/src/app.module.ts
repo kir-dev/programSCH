e541ab06843ec0d519b2843b58ae562b584c3b83
Osztrogonácz Dóra <osztrogonaczdora@gmail.com>
2024-05-21 19:38:45 +0200

    removed unnecessary dependencies and import

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
