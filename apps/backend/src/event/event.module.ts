cbe5842bcf60872900642bf4368e91bef2fa5b1f
Bakonyi -Nemes Attila <bakonyiattilaksi04@gmail.com>
2024-05-21 20:16:13 +0200

    merge conflict

--- apps/backend/src/event/event.module.ts
+++ apps/backend/src/event/event.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'nestjs-prisma';
 
 import { EventController } from './event.controller';
 import { EventService } from './event.service';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
   controllers: [EventController],
   providers: [EventService],
 })
