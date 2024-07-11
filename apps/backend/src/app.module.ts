372e2a3870aeb5cb0698ce93a8b884c6d6a2585d
Szabó Benedek <benedek.szabo2003@gmail.com>
2024-07-11 23:54:08 +0200

    use user-dto

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventModule } from './event/event.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule.forRoot({ isGlobal: true })],
+  imports: [UsersModule, EventModule, PrismaModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/users/dto/create-user.dto.ts
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -2,4 +2,4 @@ import { OmitType } from '@nestjs/swagger';
 
 import { User } from '../entities/user.entity';
 
-export class CreateUserDto extends OmitType(User, ['authSchId', 'email', 'name', 'isAdmin']) {}
+export class CreateUserDto extends OmitType(User, ['isAdmin']) {}
