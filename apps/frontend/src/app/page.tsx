1ce234f4d62d38fd8141cf4fce7e456c9d7c7c22
Sali Nóra <salinora2005@gmail.com>
2024-07-18 18:04:04 +0200

    navbar v1.0

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { HelloWorld } from '@/components/hello-world';
-
 export default function Home() {
-  return (
-    <main className='flex items-center justify-center'>
-      <HelloWorld className='mt-10' />
-    </main>
-  );
+  return <p>Page</p>;
 }
---
+++ apps/frontend/src/components/navbar.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+import styles from '@/app/globals.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className='flex'>
+        <div className={styles['logo-container']}>
+          <Image src='/logo.png' alt='Logo' width={50} height={50} title='by KirDev' />
+        </div>
+        <div className={styles['link']}>
+          <a href='/home' title='Kezdőlap'>
+            ProgramSCH
+          </a>
+        </div>
+      </div>
+      <div className='flex'>
+        <Icon href='' src='/new.png' alt='Új' title='Új esemény' />
+        <Icon href='' src='/profile.png' alt='Profil' title='Profil' />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+type IconProps = {
+  src: string;
+  href: string;
+  title: string;
+  alt: string;
+};
+
+function Icon(props: IconProps) {
+  return (
+    <div className={styles['link']}>
+      <a href={props.href}>
+        <Image src={props.src} alt={props.alt} width={50} height={50} title={props.title} />
+      </a>
+    </div>
+  );
+}
