aad3b8d90137cc38f315b31ad23d19e64e4affcf
Sali Nóra <salinora2005@gmail.com>
2024-08-07 14:44:21 +0200

    Style (code) cleaning

---
+++ apps/frontend/src/components/newEventStyles.tsx
@@ -0,0 +1,29 @@
+export const styles = {
+  StickerStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: '1rem',
+    borderWidth: '2px',
+    borderColor: '#18633d',
+    width: '41.666667%',
+    fontSize: '1.25rem',
+    backgroundColor: '#7eb397',
+    padding: '0.5rem',
+    paddingLeft: '0.75rem',
+  },
+  AlignStyle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '1.25rem',
+    marginLeft: '2.5rem',
+    width: '91.666667%',
+  },
+  InputStyle: {
+    borderRadius: '0.5rem',
+    fontStyle: 'italic',
+    width: '100%',
+    padding: '0.25rem',
+  },
+};
--- apps/frontend/src/pages/newEvent.tsx
+++ apps/frontend/src/pages/newEvent.tsx
@@ -1,3 +1,5 @@
+import { styles } from '@/components/newEventStyles';
+
 export default function newEvent() {
   return (
     <div>
@@ -5,49 +7,49 @@ export default function newEvent() {
         <div className='items-center rounded-2xl border-4 border-dark-green ml-2.5 mt-4 w-80 text-2xl text-center bg-light-green p-2'>
           Event létrehozása
         </div>
-        <div className='flex items-center justify-between mt-5 ml-10 w-11/12'>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-2 pl-3'>
+        <div style={styles.AlignStyle}>
+          <div style={styles.StickerStyle}>
             Név:
             <div className='w-3/4'>
-              <input id='name' placeholder='Gólyakocsma...' className='rounded-lg italic w-full p-1' />
+              <input id='name' placeholder='Gólyakocsma...' style={styles.InputStyle} />
             </div>
           </div>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-3'>
+          <div style={styles.StickerStyle} className='p-3'>
             Szín:
           </div>
         </div>
-        <div className='flex items-center justify-between mt-5 ml-10 w-11/12'>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-2 pl-3'>
+        <div style={styles.AlignStyle}>
+          <div style={styles.StickerStyle}>
             Helyszín:
             <div className='w-3/4'>
-              <input id='place' placeholder='SCH FNT...' className='rounded-lg italic w-full p-1' />
+              <input id='place' placeholder='SCH FNT...' style={styles.InputStyle} />
             </div>
           </div>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-2 pl-3'>
+          <div style={styles.StickerStyle}>
             Tagek:
             <div className='w-3/4'>
-              <input id='tags' placeholder='#gólyák, ...' className='rounded-lg italic w-full p-1' />
+              <input id='tags' placeholder='#gólyák, ...' style={styles.InputStyle} />
             </div>
           </div>
         </div>
-        <div className='flex items-center justify-between mt-5 ml-10 w-11/12'>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-2 pl-3'>
+        <div style={styles.AlignStyle}>
+          <div style={styles.StickerStyle}>
             Időpont:
             <div className='w-3/4'>
-              <input id='time' placeholder='éééé.hh.nn.' className='rounded-lg italic w-full p-1' />
+              <input id='time' placeholder='éééé.hh.nn. óó:pp' style={styles.InputStyle} />
             </div>
           </div>
-          <div className='flex items-center justify-between rounded-2xl border-2 border-dark-green w-5/12 text-xl bg-light-green p-2 pl-3'>
+          <div style={styles.StickerStyle}>
             Szervezők:
             <div className='w-3/4'>
-              <input id='organizers' placeholder='Kir Dev, ...' className='rounded-lg italic w-full p-1' />
+              <input id='organizers' placeholder='Kir Dev, ...' style={styles.InputStyle} />
             </div>
           </div>
         </div>
         <div className='rounded-2xl border-2 border-dark-green ml-10 mt-6 w-11/12 text-xl bg-light-green p-2 pl-3 h-64'>
           Leírás:
           <div className='mt-1 h-48 mr-1'>
-            <input id='description' placeholder='...' className='rounded-lg italic h-48 w-full p-1' />
+            <input id='description' placeholder='...' className='h-48' style={styles.InputStyle} />
           </div>
         </div>
         <div className='flex justify-between ml-10 mt-7 w-11/12'>
