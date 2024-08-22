738923a62f7cceb5d87b2c9d26e411be8168c050
Szabó Benedek <benedek.szabo2003@gmail.com>
2024-08-22 19:39:09 +0200

    using API_HOST variable where needed #2

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: process.env.FRONTEND_HOST,
       credentials: true,
     })
   );
